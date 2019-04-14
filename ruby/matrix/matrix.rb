6c1199fcfe0c6f81c2136aecd52aa77556c60371
mardiniii <sezama03@hotmail.com>
2019-04-14 16:58:48 -0400

    ruby: Third refactor to the Matrix problem solution.

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,18 +2,10 @@ class Matrix
   attr_reader :rows
 
   def initialize(string)
-    @rows = string.lines.map { |row| row.split(" ").map(&:to_i) }
+    @rows = string.lines.map { |row| row.split.map(&:to_i) }
   end
 
   def columns
-    transpose = []
-    cols = rows.first.size
-
-    cols.times.with_index do |index|
-      row = rows.map { |row| row[index] }
-      transpose.push(row)
-    end
-
-    transpose
+    rows.transpose
   end
 end
